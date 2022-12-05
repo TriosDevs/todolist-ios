ae4c1dc91f4743dd819fbfd9dc4ae0ea5020e5f4
Ardi Jorganxhi <ardijorganxhi@eskisehir.edu.tr>
2022-12-06 01:47:57 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Ardi Jorganxhi on 6.12.22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
